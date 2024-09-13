1772c85e537775b6e646db5ca7792f738ebd9af5
AlLixAI <157326564+AlLixAI@users.noreply.github.com>
2024-09-14 00:43:02 +0300

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Ожидание готовности базы данных
-until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER"; do
+until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER"; do 
   echo "Waiting for PostgreSQL..."
   sleep 2
 done
