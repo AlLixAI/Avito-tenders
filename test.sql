c9c1b96ffdf8cb25888612f45ca64aa4f505f1ec
AlLixAI <4hc011@gmail.com>
2024-09-13 21:43:55 +0300

    first commit

---
+++ test.sql
@@ -0,0 +1,51 @@
+INSERT INTO employee (id, username, first_name, last_name, created_at, updated_at)
+VALUES
+    ('550e8400-e29b-41d4-a716-446655440001', 'admin', 'John', 'Doe', NOW(), NOW());
+INSERT INTO employee (id, username, first_name, last_name, created_at, updated_at)
+VALUES
+    ('550e8400-e29b-41d4-a716-446655440002', 'legit', 'John', 'Doe', NOW(), NOW());
+INSERT INTO employee (id, username, first_name, last_name, created_at, updated_at)
+VALUES
+    ('550e8400-e29b-41d4-a716-446655440003', 'tagir', 'John', 'Doe', NOW(), NOW());
+INSERT INTO employee (id, username, first_name, last_name, created_at, updated_at)
+VALUES
+    ('550e8400-e29b-41d4-a716-446655440004', 'sanya', 'John', 'Doe', NOW(), NOW());
+INSERT INTO employee (id, username, first_name, last_name, created_at, updated_at)
+VALUES
+    ('550e8400-e29b-41d4-a716-446655440005', 'misha', 'John', 'Doe', NOW(), NOW());
+INSERT INTO employee (id, username, first_name, last_name, created_at, updated_at)
+VALUES
+    ('550e8400-e29b-41d4-a716-446655440006', 'sefer', 'John', 'Doe', NOW(), NOW());
+
+
+
+INSERT INTO organization (id, name, description, type, created_at, updated_at)
+VALUES
+    ('550e8400-e29b-41d4-a716-446655440011', 'Tech Innovations LLC', 'A leading tech company.', 'LLC', now(), now());
+INSERT INTO organization (id, name, description, type, created_at, updated_at)
+VALUES
+    ('550e8400-e29b-41d4-a716-446655440012', 'LOL ORG', 'A leading tech company.', 'IE', now(), now());
+INSERT INTO organization (id, name, description, type, created_at, updated_at)
+VALUES
+    ('550e8400-e29b-41d4-a716-446655440013', 'Noname', 'A leading tech company.', 'JSC', now(), now());
+
+
+
+INSERT INTO organization_responsible (id, organization_id, user_id)
+VALUES
+    ('550e8400-e29b-41d4-a716-446655440020', '550e8400-e29b-41d4-a716-446655440011', '550e8400-e29b-41d4-a716-446655440001');
+INSERT INTO organization_responsible (id, organization_id, user_id)
+VALUES
+    ('550e8400-e29b-41d4-a716-446655440020', '550e8400-e29b-41d4-a716-446655440011', '550e8400-e29b-41d4-a716-446655440002');
+INSERT INTO organization_responsible (id, organization_id, user_id)
+VALUES
+    ('550e8400-e29b-41d4-a716-446655440020', '550e8400-e29b-41d4-a716-446655440012', '550e8400-e29b-41d4-a716-446655440003');
+INSERT INTO organization_responsible (id, organization_id, user_id)
+VALUES
+    ('550e8400-e29b-41d4-a716-446655440020', '550e8400-e29b-41d4-a716-446655440012', '550e8400-e29b-41d4-a716-446655440004');
+INSERT INTO organization_responsible (id, organization_id, user_id)
+VALUES
+    ('550e8400-e29b-41d4-a716-446655440020', '550e8400-e29b-41d4-a716-446655440013', '550e8400-e29b-41d4-a716-446655440005');
+
+#postgres-deployment-6778489f5c-b4kd2
+#
